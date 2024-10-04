c699ccd4059e4861294a29850dc8e49af35fffd0
Matt Kissinger <matt.m.kissinger@gmail.com>
2024-10-04 00:09:47 -0400

    utf-8 fix

--- src/utils/file_operations.py
+++ src/utils/file_operations.py
@@ -4,7 +4,7 @@ import discord
 
 def load_from_file(file_path):
     if os.path.exists(file_path):
-        with open(file_path, 'r') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             return f.read().strip()
     return ""
 
